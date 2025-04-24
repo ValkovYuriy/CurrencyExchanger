4552e7aea5cc7cc6966f4d028b0fa3520ec26d74
User <valyur0320@gmail.com>
2025-04-24 09:41:41 +0400

    init

---
+++ CashBalanceService/src/main/java/yuriy/dev/cashbalanceservice/CashBalanceServiceApplication.java
@@ -0,0 +1,13 @@
+package yuriy.dev.cashbalanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashBalanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashBalanceServiceApplication.class, args);
+	}
+
+}
---
+++ CashBalanceService/src/test/java/yuriy/dev/cashbalanceservice/CashBalanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yuriy.dev.cashbalanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashBalanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
